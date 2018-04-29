dabd36760aa59bef46efd39eebe484b63e4d7cb9
shamsk22 <32582082+shamsk22@users.noreply.github.com>
2018-04-29 10:27:56 +0530

    Update cloudwatch-alarms.sh

--- scripts/cloudwatch-alarms.sh
+++ scripts/cloudwatch-alarms.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
-#===========================================================================================================================
+#===================================================================================
 #
 # FILE: create-cloudwatch-alarms.sh
-# Pre-requisite - Disk and Mem Utlization Custom metrices should be implemented first in order to create Alarm on them.
-# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/mon-scripts.html
-# DESCRIPTION: Script creates 4 alarms for instance with PRIVATE_IP
+#
+# USAGE: create-cloudwatch-alarms.sh <IP-ADDRESS>
+#
+# DESCRIPTION: Script creates 2 alarms for instance with PRIVATE_IP
 # It does follow steps:
 # 1. Creates high cpu usage alarm
 # 2. Creates instance statusCheck alarm
-# 3. Create Alarm to check disk utilization
-# 4. Create Alarm on Memory utilization
-#============================================================================================================================
+#
+#===================================================================================
 # Get instance id and name tag
-
 echo "Running cloudwatch-alarms.sh"
+
 envVar=$1
 
 if [ "${envVar}" = "Pre-prod" ]
- then
+then
+
 PRIVATE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-INSTANCE_NAME=$(ec2-describe-tags    --region      $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone  | sed -e "s/.$//")    --filter      resource-id=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id) | head -1 | awk '{print $5}')
+INSTANCE_NAME=$(ec2-describe-tags --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone  | sed -e "s/.$//") --filter resource-id=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id) | head -1 | awk '{print $5}')
 
 echo "${PRIVATE_IP}" | grep -q -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
 if [ $? -ne 0 ]
@@ -29,12 +30,12 @@ then
     echo "### IP address have to be presented in format X.X.X.X"
     exit 1
 fi
-fi
+
 # Trying auto-detect AWS region
 AWS_DEFAULT_REGION=$(curl -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/\([1-9]\).$/\1/g')
 
 # Export environment variables for awscli
-export AWS_DEFAULT_REGION
+export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-ap-southeast-1}"
 export AWS_DEFAULT_OUTPUT="text"
 
 if [ "${INSTANCE_NAME}" = "None" ]
@@ -42,8 +43,8 @@ then
     echo "### Could not find instance with IP ${PRIVATE_IP}"
     exit 1
 fi
-
-# 1) Create high CPU usage metric
+fi
+# 2) Create high CPU usage metric
 ARN_OF_SNS_TOPIC="arn:aws:sns:us-west-2:953030164212:SNS"
 CPU_USAGE=70
 
@@ -64,7 +65,7 @@ aws cloudwatch put-metric-alarm \
     --evaluation-periods 1\
     --unit Percent
 
-# 2) Create status check metric
+# 3) Create status check metric
 aws cloudwatch put-metric-alarm \
     --alarm-name "${INSTANCE_NAME}-status"\
     --alarm-description "Alarm when statusCheck failed"\
